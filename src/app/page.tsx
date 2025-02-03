05ba292b45b38beb23c47c1d0cb630f405ebb328
ivan <ivanchan2711@gmail.com>
2025-02-03 21:28:28 +1100

    real

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white">
-      <h1 className="text-8xl font-bold">Metronome App</h1>
+      <h1 className="text-8xl font-bold">ELTON LIM GAY</h1>
     </div>
   );
 }
